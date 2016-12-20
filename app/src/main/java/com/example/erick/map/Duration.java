e3bce8b13893fb809c72f79be3ce587928a71de2
Cesar Hernandez <cesar.herlop@yahoo.com.mx>
2016-12-20 16:39:24 -0600

    ejemplo google maps

---
+++ app/src/main/java/com/example/erick/map/Duration.java
@@ -0,0 +1,13 @@
+package com.example.erick.map;
+
+public class Duration {
+
+    public String text;
+    public int value;
+
+    public Duration(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+
+}
